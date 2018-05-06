0eed1b8dfbf0a528982ec81c7f0cc48a263c2ee6
nuonuo <848540949@qq.com>
2018-05-06 23:10:23 +0800

    1.ContentProvider 测试Module提交

---
+++ providerdemo/readme.md
@@ -0,0 +1,11 @@
+1. 项目结构
+>
+|____BaseTester.java //contenprovider测试类
+|____MonitoredDebugActivity.java //Menu实现与页面状态保存测试
+|____BookProviderMetaData.java //常量 
+|____BookProvider.java 
+|____ProviderTester.java
+|____MainActivity.java
+|____MyApplication.java
+|____MonitoredActivity.java
+|____IReportBack.java //测试类父类（后面有其它类型的测试类）
---
+++ providerdemo/src/main/java/com/example/nuonuo/providerdemo/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.nuonuo.providerdemo;
+
+import android.util.Log;
+import android.view.MenuItem;
+
+public class MainActivity extends BaseActivity
+implements IReportBack
+{
+	public static final String tag="MainActivity";
+	private ProviderTester providerTester = null;
+
+	public MainActivity()
+	{
+
+		super(R.menu.main_menu,tag);
+		this.retainState();
+		providerTester = new ProviderTester(this,this);
+	}
+
+    protected boolean onMenuItemSelected(MenuItem item)
+    {
+    	Log.d(tag,item.getTitle().toString());
+    	if (item.getItemId() == R.id.menu_add)
+    	{
+    		providerTester.addBook();
+    		return true;
+    	}
+    	if (item.getItemId() == R.id.menu_display_table)
+    	{
+    		providerTester.showBooks();
+    		return true;
+    	}
+    	if (item.getItemId() == R.id.menu_delete)
+    	{
+    		providerTester.removeBook();
+    		return true;
+    	}
+    	return true;
+    }   
+}
